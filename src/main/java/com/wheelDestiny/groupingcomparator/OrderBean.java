11fe961fb5049461b79f363460210e4f48d8d4db
wheelDestiny <672845190@qq.com>
2020-08-22 11:33:51 +0800

    add message

---
+++ src/main/java/com/wheelDestiny/groupingcomparator/OrderBean.java
@@ -0,0 +1,63 @@
+package com.wheelDestiny.groupingcomparator;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class OrderBean implements WritableComparable<OrderBean> {
+    private String orderId;
+    private String productId;
+    private double price;
+
+    @Override
+    public String toString() {
+        return orderId + '\t' + productId + '\t'+price;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeUTF(orderId);
+        dataOutput.writeUTF(productId);
+        dataOutput.writeDouble(price);
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.orderId = dataInput.readUTF();
+        this.productId = dataInput.readUTF();
+        this.price = dataInput.readDouble();
+
+    }
+
+    @Override
+    public int compareTo(OrderBean o) {
+        return this.orderId.compareTo(o.orderId)==0?Double.compare(o.price,this.price):this.orderId.compareTo(o.orderId);
+    }
+}
---
+++ src/main/java/com/wheelDestiny/groupingcomparator/OrderDriver.java
@@ -0,0 +1,4 @@
+package com.wheelDestiny.groupingcomparator;
+
+public class OrderDriver {
+}
