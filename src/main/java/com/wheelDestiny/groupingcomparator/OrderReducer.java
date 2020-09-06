3d9c5b3295d37a10aa6e378aef2c0bff05752a8c
wheelDestiny <672845190@qq.com>
2020-09-07 00:52:41 +0800

    add message

--- src/main/java/com/wheelDestiny/groupingcomparator/OrderReducer.java
+++ src/main/java/com/wheelDestiny/groupingcomparator/OrderReducer.java
@@ -14,6 +14,7 @@ public class OrderReducer extends Reducer<OrderBean, NullWritable,OrderBean,Null
 
         for (int i = 0; i <2 ; i++) {
             if (iterator.hasNext()) {
+                //即使根据key分区，但是实际上迭代器中value对应的key其实还是原来对应的key，所以可以根据迭代器获取同一分区中所有的key
                 context.write(key,iterator.next());
             }
         }
