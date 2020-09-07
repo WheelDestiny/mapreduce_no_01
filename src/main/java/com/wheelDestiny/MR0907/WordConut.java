22db54e8e607ef44c6eac84a92e9836fe5e5ec6e
wheelDestiny <672845190@qq.com>
2020-09-08 00:39:35 +0800

    add message

---
+++ src/main/java/com/wheelDestiny/MR0907/WordConut.java
@@ -0,0 +1,76 @@
+package com.wheelDestiny.MR0907;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.IOException;
+
+public class WordConut {
+    public static class WordCountMapper extends Mapper<LongWritable, Text,Text,LongWritable> {
+        private Text k = new Text();
+        private LongWritable v = new LongWritable(1);
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] s = value.toString().split(" ");
+
+            for (String ss : s) {
+                k.set(ss);
+                context.write(k,v);
+            }
+
+        }
+    }
+    public static class WordCountRedece extends Reducer<Text,LongWritable,Text,LongWritable>{
+        private LongWritable v = new LongWritable();
+        private long sum = 0;
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+            sum =0;
+            values.forEach((v)->{
+                sum++;
+            });
+            v.set(sum);
+            context.write(key,v);
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        Job job = Job.getInstance(configuration,"WordCount");
+
+        job.setJarByClass(WordConut.class);
+
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountRedece.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
+//        job.setNumReduceTasks(2);
+
+        FileInputFormat.setInputPaths(job,new Path("D:\\input"));
+
+        FileSystem fs = FileSystem.get(configuration);
+        Path out = new Path("D:\\output");
+        if(fs.exists(out)){
+            //递归删除
+            fs.delete(out,true);
+        }
+
+        FileOutputFormat.setOutputPath(job,out);
+
+        boolean b = job.waitForCompletion(true);
+        System.exit(b?0:1);
+
+    }
+}
